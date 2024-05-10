d77548f850305e734cc8ea9b72357da63159ae0b
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-05-09 21:03:27 -0700

    state setup works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::{
     extract::{
         ws::{Message, WebSocket, WebSocketUpgrade},
-        Query,
+        Query, State,
     }, http::StatusCode, response::IntoResponse, routing::get, Router
 };
 use axum_extra::TypedHeader;
@@ -21,12 +21,20 @@ use axum::extract::connect_info::ConnectInfo;
 use axum::extract::ws::CloseFrame;
 
 //allows to split the websocket stream into separate TX and RX branches
-use futures::{sink::SinkExt, stream::StreamExt};
+use futures::{sink::SinkExt, stream::StreamExt, TryStreamExt};
 
 #[derive(Deserialize, Debug)]
 struct LivekitRoom {
     room_id: String,
 }
+
+
+#[derive(Clone, Default)]
+pub struct AppState {
+  user_transcript: Option<async_channel::Receiver<String>>,
+  backend_messages: Option<async_channel::Receiver<String>>,
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -38,13 +46,16 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let state = AppState::default();
+
     let app = Router::new()
         .route("/", get(ws_handler))
         // logging so we can see whats going on
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-        );
+        )
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -70,6 +81,7 @@ async fn ws_handler(
     user_agent: Option<TypedHeader<headers::UserAgent>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     room_details: Option<Query<LivekitRoom>>,
+    State(mut state): State<AppState>
 ) -> impl IntoResponse {
     let _room_id = match room_details{
         Some(e) => e.room_id.clone(),
@@ -82,13 +94,20 @@ async fn ws_handler(
         String::from("Unknown browser")
     };
     println!("`{user_agent}` at {addr} connected.");
+
+    let (tx1, rx1) = async_channel::unbounded::<String>();
+    let (tx2, rx2) = async_channel::unbounded::<String>();
+
+    state.backend_messages = Some(rx1);
+    state.user_transcript = Some(rx2);
+
     // finalize the upgrade process by returning upgrade callback.
     // we can customize the callback by sending additional info such as address.
-    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+    ws.on_upgrade(move |socket| handle_socket(socket, addr, state))
 }
 
 /// Actual websocket statemachine (one will be spawned per connection)
-async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: AppState) {
     // send a ping (unsupported by some browsers) just to kick things off and get a response
     if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
         println!("Pinged {who}...");
@@ -103,6 +122,7 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
     // this will likely be the Pong for our Ping or a hello message from client.
     // waiting for message from a client will block this task, but will not block other client's
     // connections.
+
     if let Some(msg) = socket.recv().await {
         if let Ok(msg) = msg {
             if process_message(msg, who).is_break() {
@@ -114,83 +134,29 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
         }
     }
 
-    // Since each client gets individual statemachine, we can pause handling
-    // when necessary to wait for some external event (in this case illustrated by sleeping).
-    // Waiting for this client to finish getting its greetings does not prevent other clients from
-    // connecting to server and receiving their greetings.
-    for i in 1..5 {
-        if socket
-            .send(Message::Text(format!("Hi {i} times!")))
-            .await
-            .is_err()
-        {
-            println!("client {who} abruptly disconnected");
-            return;
-        }
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    }
-
-    // By splitting socket we can send and receive at the same time. In this example we will send
-    // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
     let (mut sender, mut receiver) = socket.split();
 
-    // Spawn a task that will push several messages to the client (does not matter what client does)
-    let mut send_task = tokio::spawn(async move {
-        let n_msg = 20;
-        for i in 0..n_msg {
-            // In case of any websocket error, we exit.
-            if sender
-                .send(Message::Text(format!("Server message {i} ...")))
-                .await
-                .is_err()
-            {
-                return i;
-            }
+    let backend_msgs = match state.backend_messages{
+        Some(e) => e,
+        None => panic!("testing testing "),
+    };
+    
+    let user_transcript = match state.user_transcript {
+        Some(e) => e,
+        None => panic!("testing testing "),
+    };
 
-            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+    loop {
+        while let Ok(e) = backend_msgs.try_recv(){
+            sender.send(Message::Text(format!("Server message | hello ..."))).await;
         }
-
-        println!("Sending close to {who}...");
-        if let Err(e) = sender
-            .send(Message::Close(Some(CloseFrame {
-                code: axum::extract::ws::close_code::NORMAL,
-                reason: Cow::from("Goodbye"),
-            })))
-            .await
-        {
-            println!("Could not send Close due to {e}, probably it is ok?");
+        
+        while let Ok(e) = user_transcript.try_recv(){
+            sender.send(Message::Text(format!("Server message | hello ..."))).await;
         }
-        n_msg
-    });
-
-    // This second task will receive messages from client and print them on server console
-    let mut recv_task = tokio::spawn(async move {
-        let mut cnt = 0;
-        while let Some(Ok(msg)) = receiver.next().await {
-            cnt += 1;
-            // print message and break if instructed to do so
-            if process_message(msg, who).is_break() {
-                break;
-            }
-        }
-        cnt
-    });
-
-    // If any one of the tasks exit, abort the other.
-    tokio::select! {
-        rv_a = (&mut send_task) => {
-            match rv_a {
-                Ok(a) => println!("{a} messages sent to {who}"),
-                Err(a) => println!("Error sending messages {a:?}")
-            }
-            recv_task.abort();
-        },
-        rv_b = (&mut recv_task) => {
-            match rv_b {
-                Ok(b) => println!("Received {b} messages"),
-                Err(b) => println!("Error receiving messages {b:?}")
-            }
-            send_task.abort();
+        
+        while let Ok(Some(e)) = receiver.try_next().await {
+            sender.send(Message::Text(format!("Server message | {e:?} ..."))).await;
         }
     }
 
